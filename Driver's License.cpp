198d9a167e6e780779b245683088f565d16b3c14
zorone <imron.jehleh@gmail.com>
2020-02-08 10:16:03 +0700

    1.0a02

--- Driver's License.cpp
+++ Driver's License.cpp
@@ -1,92 +1,46 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include<ctype.h>
 
 int main() {
-    char people[2][5][10];
-    int AvailableCustomer;
-    char temp;
-    scanf("%s", people[0][0]);
-    scanf("%d", &AvailableCustomer);
-    scanf("%s %s %s %s", people[0][1], people[0][2], people[0][3], people[0][4]);
-    
-	printf("%s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-	         
+    char people[5][10];
+    int people1[2][5];
+    int AvaibleCustomer;
+    int temp;
+    scanf("%s", people[0]);
+    scanf("%d", &AvaibleCustomer);
+    scanf("%s %s %s %s", people[1], people[2], people[3], people[4]);
     for(int i=0; i<5; i++){
-        for(int j=0; people[0][i][j]!='\0'; j++){
-            people[0][i][j]=tolower(people[0][i][j]);
+        for(int j=0; people[i][j]!='\0'; j++){
+            people[i][j]=tolower(people[i][j]);
         }
     }
-    
-	printf("%s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-	         
-    strcpy(people[1][0], people[0][0]);
-    
-    printf("%s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-    for (int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
-            for(int k=0; k<10; k++){
-                if(people[0][i][k]<people[0][j][k]){
-                    for(int l=0; l<10; l++){
-                        temp=people[0][i][l];
-                        people[0][i][l]=people[0][j][l];
-                        people[0][j][l]= temp;
-                        
-                        printf("s %s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-	         
-                    }
-                }
-                else if(people[0][i][k]>people[0][j][k]){
-                	
-                	printf("b %s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-	         
-                    break;
-                }
-                else if (people[0][i][k]=='\0'){
-                	
-                	printf("c %s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-	         
-                    break;
-                }
-                else if (people[0][j][k]=='\0'){
-                	
-                	for(int l=0; l<10; l++){
-                        temp=people[0][i][l];
-                        people[0][i][l]=people[0][j][l];
-                        people[0][j][l]= temp;
-                        
-                        printf("s %s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-	         
-                    }
-                }
-                else{
-                	
-                	printf("c %s %s %s %s %s %s %d %d\n", people[0][0], people[0][1], people[0][2],
-	         people[0][3], people[0][4], people[1][0], AvailableCustomer, temp);
-	         
-                    continue;
-                }
+    for (int i=0; i<4; i++){
+        for (int j=0; people[i][j]!='\0';j++){
+            for (int k=0; k<10-j; k++){
+                people1[0][i]+= people[i][j];
             }
         }
     }
-    
-    /*for (int i=0; i<5; i++){
-        if (people[1][i][0]==people[1][0][0]){
-            if(i%AvailableCustomer==0){
-            temp=(i)/AvailableCustomer*20;
+    people1[1][0]= people1[0][0];
+    for (int i=0; i<5; i++){
+        for (int j=0; j<i; j++){
+            if(people1[0][i]<=people1[0][j]){
+                temp=people1[0][i];
+                people1[0][i]=people1[0][j];
+                people1[0][j]=temp;
+            }
+        }
+    }
+    for (int i=0; i<5; i++){
+        if (people1[0][i]==people1[1][0]){
+            if(i%AvaibleCustomer==0){
+            temp=(i)/AvaibleCustomer*20;
             }
             else{
-                temp=(i+1)/AvailableCustomer*20;
+                temp=(i+1)/AvaibleCustomer*20;
             }
         }
     }
-    printf("%d", temp);*/
+    printf("%d", temp);
     return 0;
 }

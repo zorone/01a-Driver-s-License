cf93e1c84b15d47484151ac475593a0e810b3adc
zorone <imron.jehleh@gmail.com>
2020-02-08 10:08:24 +0700

    0.0.3

--- Driver's License.cpp
+++ Driver's License.cpp
@@ -2,43 +2,40 @@
 #include <math.h>
 
 int main() {
-    char people[2][5][10];
+    char people[5][10];
+    int people1[2][5];
     int AvaibleCustomer;
     int temp;
-    scanf("%s", people[0][0]);
+    scanf("%s", people[0]);
     scanf("%d", &AvaibleCustomer);
-    scanf("%s %s %s %s", people[0][1], people[0][2], people[0][3], people[0][4]);
+    scanf("%s %s %s %s", people[1], people[2], people[3], people[4]);
     for (int i=0; i<4; i++){
-        for (int j=0; people[0][i][j]!='\0';j++){
+        for (int j=0; people[i][j]!='\0';j++){
             for (int k=0; k<10-j; k++){
-                people[1][i][0]+= people[0][i][j];
+                people1[0][i]+= people[i][j];
             }
         }
     }
-    people[1][0][1]= people[1][0][0];
+    people1[1][0]= people1[0][0];
     for (int i=0; i<5; i++){
         for (int j=0; j<i; j++){
-            if(people[1][i][0]<=people[1][j][0]){
-                temp=people[1][i][0];
-                people[1][i][0]=people[1][j][0];
-                people[1][j][0]=temp;
+            if(people1[0][i]<=people1[0][j]){
+                temp=people1[0][i];
+                people1[0][i]=people1[0][j];
+                people1[0][j]=temp;
             }
         }
     }
     for (int i=0; i<5; i++){
-        if (people[1][i][0]==people[1][0][1]){
-            if(people[1][i][0]%AvaibleCustomer==0){
-            temp=(people[1][i][0])/AvaibleCustomer*20;
+        if (people1[0][i]==people1[1][0]){
+            if(i%AvaibleCustomer==0){
+            temp=(i)/AvaibleCustomer*20;
             }
             else{
-                temp=(people[1][i][0]+1)/AvaibleCustomer*20;
+                temp=(i+1)/AvaibleCustomer*20;
             }
         }
     }
     printf("%d", temp);
     return 0;
 }
-
-
-
-

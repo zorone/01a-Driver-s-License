cd51100d59b57b30057fa094ec347959cd378ea3
Zorone <imron.jehleh@gmail.com>
2020-02-11 13:07:14 +0700

    1.0.0
    Edit some code.

--- Driver's License.cpp
+++ Driver's License.cpp
@@ -1,49 +1,50 @@
 #include <stdio.h>
-#include<ctype.h>
+#include <ctype.h>
+#include <math.h>
 
 int main() {
-    char people[2][5][10];
-    int AvaibleCustomer;
-    int temp;
-    scanf("%s", people[0][0]);
-    scanf("%d", &AvaibleCustomer);
-    scanf("%s %s %s %s", people[0][1], people[0][2], people[0][3], people[0][4]);
+    char people[5][10];
+    double people1[2][5];
+    int AvailableCustomer;
+    double temp;
+    scanf("%s", people[0]);
+    scanf("%d", &AvailableCustomer);
+    scanf("%s %s %s %s", people[1], people[2], people[3], people[4]);
     for(int i=0; i<5; i++){
         for(int j=0; people[i][j]!='\0'; j++){
-            people[0][i][j]=tolower(people[0][i][j]);
+            people[i][j]=tolower(people[i][j]);
         }
     }
-    people[1][0][0]=people[0][0][0];
+
+    for (int i=0; i<4; i++){
+        for (int j=0; people[i][j]!='\0';j++){
+            people1[0][i] += (people[i][j] - 96)* pow (26, 10-j);
+        }
+    }
+    people1[1][0]= people1[0][0];
     for (int i=0; i<5; i++){
-        for(int j=0; j<i; j++){
-            for(int k=0; people[0][i][k]!='\0'||people[0][j][k]!='\0'; k++){
-                if(people[0][i][k]<people[0][j][k]){
-                    for(int l=0; people[0][i][l]!='\0'&&people[0][j][l]!='\0'; l++){
-                        temp=people[0][i][l];
-                        people[0][i][l]=people[0][j][l];
-                        people[0][j][l]= temp;
-                    }
-                }
-                else if(people[0][i][k]>people[0][j][k]){
-                    break;
-                }
-                else{
-                    continue;
-                }
+        for (int j=0; j<i; j++){
+            if(people1[0][i]<=people1[0][j]){
+                temp=people1[0][i];
+                people1[0][i]=people1[0][j];
+                people1[0][j]=temp;
             }
         }
     }
-    
+
     for (int i=0; i<5; i++){
-        if (people1[0][0][i]==people1[1][0]){
-            if(i%AvaibleCustomer==0){
-            temp=(i)/AvaibleCustomer*20;
+        if (people1[0][i]==people1[1][0]){ 
+            if((i+1)%AvailableCustomer==0){
+                temp = (i+1)/AvailableCustomer*20;
+            }
+            else if(AvailableCustomer==1){
+                temp = (i+1)*20;
             }
             else{
-                temp=(i+1)/AvaibleCustomer*20;
+                temp = ((i+1)/AvailableCustomer+1)*20;
             }
+            printf("%.0lf", temp);
         }
     }
-    printf("%d", temp);
     return 0;
 }
